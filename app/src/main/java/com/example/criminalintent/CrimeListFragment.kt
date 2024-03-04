23b835994fba47525e8c984ebd71dc5a2922cd21
lpep64 <lukepepin@outlook.com>
2024-03-04 10:56:19 -0500

    Crime List Fragment Creation and Adjustments

---
+++ app/src/main/java/com/example/criminalintent/CrimeListFragment.kt
@@ -0,0 +1,46 @@
+package com.example.criminalintent
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.criminalintent.databinding.FragmentCrimeListBinding
+
+private const val TAG = "CrimeListFragment"
+
+class CrimeListFragment : Fragment() {
+    private val crimeListViewModel : CrimeListViewModel by viewModels()
+
+    private var _binding : FragmentCrimeListBinding? = null
+    private lateinit var crime: Crime
+    //private lateinit var binding : FragmentCrimeDetailBinding
+    private val binding
+        get() = checkNotNull(_binding){
+            "Binding is null. Can you see the view?"
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "Total crime: ${crimeListViewModel.crimes.size}")
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentCrimeListBinding.inflate(layoutInflater, container, false)
+        binding.crimeRecyclerView.layoutManager = LinearLayoutManager(context)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
